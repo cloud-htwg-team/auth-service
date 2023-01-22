8125c2cc924aae4400a92dec65940fb7702dae1b
Adam Radvan <radvan@inqool.cz>
2023-01-22 03:12:50 +0100

    clean up

--- auth/src/main/java/de/htwg/cloud/qrcode/app/auth/AuthenticationService.java
+++ auth/src/main/java/de/htwg/cloud/qrcode/app/auth/AuthenticationService.java
@@ -32,7 +32,7 @@ public class AuthenticationService {
             @Value("${google.cloud.identity-platform.api-key}") String apiKey
     ) {
         googleCloudIdentityProviderApiKey = apiKey;
-        log.info(googleCloudIdentityProviderApiKey);
+        // log.info(googleCloudIdentityProviderApiKey);
     }
 
     public UserInfoDto login(LoginDto loginDto) throws URISyntaxException, IOException, InterruptedException {
